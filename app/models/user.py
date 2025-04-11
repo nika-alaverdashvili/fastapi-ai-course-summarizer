0ded5e91bc8e031b40292d38f12b25e105bcc90c
nika-alaverdashvili <nikoloz.alaverdashvili.1@btu.edu.ge>
2025-04-11 23:28:02 +0400

    feat: add user db model.

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+import uuid
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime, timezone
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = datetime.now(timezone.utc)
