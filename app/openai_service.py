afad85b3e4d1a3066ac3549e08b304becc061f04
nika-alaverdashvili <nikoloz.alaverdashvili.1@btu.edu.ge>
2025-04-13 11:30:29 +0400

    feat: implement sync-based ppenai service for course summarization.

---
+++ app/openai_service.py
@@ -0,0 +1,37 @@
+import httpx
+import logging
+from app.settings import settings
+
+logger = logging.getLogger(__name__)
+OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
+
+def generate_course_summary_sync(course_description: str) -> str:
+    headers = {
+        "Authorization": f"Bearer {settings.OPENAI_API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "model": settings.OPENAI_MODEL,
+        "messages": [
+            {"role": "user", "content": f"Summarize this online course: {course_description}"}
+        ]
+    }
+
+    for attempt in range(3):
+        try:
+            with httpx.Client(timeout=30) as client:
+                response = client.post(OPENAI_API_URL, headers=headers, json=data)
+                response.raise_for_status()
+                content = response.json()["choices"][0]["message"].get("content")
+                if not content:
+                    raise ValueError("OpenAI response content is missing")
+                return content
+        except (httpx.ReadTimeout, httpx.ConnectTimeout):
+            logger.warning(f"[OpenAI] Timeout on attempt {attempt + 1}")
+            if attempt < 2:
+                continue
+            raise
+        except httpx.HTTPError as e:
+            logger.error(f"[OpenAI] HTTP error: {e}")
+            raise
--- app/settings.py
+++ app/settings.py
@@ -5,10 +5,15 @@ load_dotenv()
 
 
 class Settings:
+    # aurh
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 2))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 200))
     REFRESH_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 15))
+
+    # openai
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    OPENAI_MODEL: str = os.getenv("OPENAI_MODEL", "gpt-4o")
 
 
 settings = Settings()
